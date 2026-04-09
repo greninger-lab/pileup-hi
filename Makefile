21749d30932c1f23c3a3ef7a0a17adf0439ce8b8
epiliper <epiliper@outlook.com>
2026-04-09 12:25:38 -0700

    build: vendors dependencies

--- Makefile
+++ Makefile
@@ -2,7 +2,6 @@ build_test:
 	cargo build && cp ./target/debug/pileuphi ~/.cargo/bin/pileuphi
 
 build:
-	# cargo build --release && cp ./target/release/indel_left_align ~/.cargo/bin/indel
 	cargo build --release && cp ./target/release/pileuphi ~/.cargo/bin/pileuphi
 
 test:
--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
 fn main() {
     cc::Build::new()
         .file("src/overlap_wrapper.c")
-        .includes(
-            pkg_config::Config::new().probe("htslib")
-            .expect("failed to find htslib installation on this system").include_paths
-            )
+        .includes(vec!["./vendor/hts-sys/"])
+        .includes(vec!["./vendor/hts-sys/htslib/"])
+        // pkg_config::Config::new().probe("htslib")
+        // .expect("failed to find htslib installation on this system").include_paths
+        // )
         .opt_level(3)
         .flag_if_supported("-march=native")
         .compile("overlap_wrapper");
